72344ad23601fe09a0e47c361956d6cc363f4853
ntsyaa324 <145531292+ntsyaa324@users.noreply.github.com>
2024-08-27 13:53:05 -1200

    Add files via upload
    Tugas Pertemuan 2

---
+++ Pertemuan2soal1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan2soal1;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Pertemuan2soal1 {
+
+    public static void main(String[] args){
+        int a=50;
+        float b=a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ Pertemuan2soal2.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan2soal2;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Pertemuan2soal2 {
+
+    public static String MakanandanMinumanFavorit;
+    
+    
+    public static void makanandanminuman(){
+        String MakananFavorit = "Ayam Geprek";
+        String MinumanFavorit = "Jus Jambu";
+        
+        
+        System.out.println("Makanan Favorit Saya : Ayam Geprek");
+        System.out.println("Minuman Favorit Saya : Jus Jambu");
+    }
+    
+    
+    public static void main(String[] args){
+        
+        Pertemuan2soal2 latihan = new Pertemuan2soal2 ();
+        
+        latihan.makanandanminuman();
+    }
+}
