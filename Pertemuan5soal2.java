5bfb90dd28628017eed84f95622e78f281f57e87
ntsyaa324 <145531292+ntsyaa324@users.noreply.github.com>
2024-08-27 20:21:25 -1200

    Add files via upload

--- Pertemuan5soal2.java
+++ Pertemuan5soal2.java
@@ -11,6 +11,10 @@ package com.mycompany.pertemuan5soal2;
 public class Pertemuan5soal2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        double alas = 17; double tinggi= 11;
+        double luas = (alas*tinggi)/2;
+        
+        System.out.println("NilaiAlasdanTinggi:" + luas);
     }
 }
