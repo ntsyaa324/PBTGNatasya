5e647c76f117558a70b736c948549f914b5e7d28
ntsyaa324 <145531292+ntsyaa324@users.noreply.github.com>
2024-08-27 20:19:17 -1200

    Add files via upload

--- Pertemuan4soal2.java
+++ Pertemuan4soal2.java
@@ -11,6 +11,16 @@ package com.mycompany.pertemuan4soal2;
 public class Pertemuan4soal2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int w, x, y, z;
+        w = 5;
+        x = 6;
+        y = 8-x++;
+        z = 8-++w;
+        
+        System.out.println(w);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
     }
+    
 }
