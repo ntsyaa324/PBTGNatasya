29c32be06e18a45a14b58a8d759b6742aa179e04
ntsyaa324 <145531292+ntsyaa324@users.noreply.github.com>
2024-08-27 20:51:58 -1200

    Add files via upload

---
+++ Pertemuan6soal1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan6soal1;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Pertemuan6soal1 {
+
+    public static void main(String[] args) {
+        
+        int NIM = 1111;
+        int Nilai = 85;
+        
+        
+        if(NIM>= 1111){
+             System.out.println("grade A");
+             
+        }
+        
+        else if(NIM>= 1111 && Nilai<85)
+            System.out.println("grade A");
+    }
+}
+
