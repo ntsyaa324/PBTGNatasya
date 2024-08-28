bcaf369362b93da318f46f54238d9c596a74b2b6
ntsyaa324 <145531292+ntsyaa324@users.noreply.github.com>
2024-08-27 17:43:02 -1200

    Add files via upload
    pertemuan3 soal1

---
+++ Pertemuan3soal1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan3soal1;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Pertemuan3soal1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int varl = 9;
+        float var2 = 8.99f;
+        char var3 = 'A';
+        boolean var4 = false;
+        String var5 = "Hello world";
+    }
+}
