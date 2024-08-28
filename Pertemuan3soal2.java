e992c8800db379bd9dbc2885d0ee1c2c66f1326c
ntsyaa324 <145531292+ntsyaa324@users.noreply.github.com>
2024-08-27 20:17:39 -1200

    Add files via upload

--- Pertemuan3soal2.java
+++ Pertemuan3soal2.java
@@ -11,6 +11,19 @@ package com.mycompany.pertemuan3soal2;
 public class Pertemuan3soal2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int NIM = 2110117000;
+        String NamaMahasiswa = "Andika Jaya Utama";
+        String Kelas = "01SIFE020";
+        double Usia = 19.5;
+        double Tinggi = 1.72;
+        
+        System.out.println("NIM : " + NIM);
+        System.out.println("NamaMahasiswa : " + NamaMahasiswa);
+        System.out.println("Kelas : " + Kelas);
+       System.out.println("Usia : " + Usia);
+       System.out.println("Tinggi : " + Tinggi);
+       
     }
+       
+    
 }
