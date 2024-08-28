327a33514e5f82912d9fd51577ecc3757fb1b5be
ntsyaa324 <145531292+ntsyaa324@users.noreply.github.com>
2024-08-27 20:20:27 -1200

    Add files via upload

--- Pertemuan5soal1.java
+++ Pertemuan5soal1.java
@@ -6,11 +6,26 @@ package com.mycompany.pertemuan5soal1;
 
 /**
  *
- * @author PC RPL - R1
+ * @author Natasya
  */
 public class Pertemuan5soal1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+    String NamaKaryawan = "iyan";
+   String Alamat = "Ampenan";
+   String Usia = "20 Tahun";
+   String Gaji = "Rp 10000000";
+   
+   
+   System.out.println("Nama Karyawan:" + NamaKaryawan);
+   System.out.println("Alamat:" + Alamat);
+   System.out.println("Usia:" + Usia);
+   System.out.println("Gaji:" + Gaji);
+    
     }
 }
+
+    
+    
+    
